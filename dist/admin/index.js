59bee7a50ed773b4c67e489c01087605134ed443
Mahmoud <mahmoudaliofficial@hotmail.com>
2023-04-08 10:39:59 +0300

    fix:react

--- dist/admin/index.js
+++ dist/admin/index.js
@@ -6,8 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore - need to do this because this file doesn't actually exist
 const react_1 = __importDefault(require("react"));
-const react_dom_1 = __importDefault(require("react-dom"));
+const react_dom_1 = require("react-dom");
+require("./index.css");
 const Root_1 = __importDefault(require("./Root"));
-react_dom_1.default.render(react_1.default.createElement(react_1.default.StrictMode, null,
-    react_1.default.createElement(Root_1.default, null)), document.getElementById('root'));
+const root = document.getElementById("app");
+(0, react_dom_1.render)(react_1.default.createElement(Root_1.default, null), root);
 //# sourceMappingURL=index.js.map
--- src/admin/index.tsx
+++ src/admin/index.tsx
@@ -1,13 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore - need to do this because this file doesn't actually exist
-import React from 'react';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./Root";
 
-import ReactDOM from 'react-dom';
-import Root from './Root';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
- document.getElementById('root')
-);
+const root = document.getElementById("app");
+render(<App />, root);
