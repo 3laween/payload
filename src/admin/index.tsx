79a66cefc8c3e0d65826675c00510411de28fadc
Mahmoud <mahmoudaliofficial@hotmail.com>
2023-04-08 10:52:08 +0300

    fix:wrong import

--- src/admin/index.tsx
+++ src/admin/index.tsx
@@ -2,7 +2,6 @@
 // @ts-ignore - need to do this because this file doesn't actually exist
 import React from "react";
 import { render } from "react-dom";
-import "./index.css";
 import App from "./Root";
 
 const root = document.getElementById("app");
