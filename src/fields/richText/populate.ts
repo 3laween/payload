4cf4176a2008387b9094fd30d99df24f9019a865
NawafSwe <nawaf.jr9@hotmail.com>
2023-04-08 17:36:09 +0300

    feat: inject select in populate.ts

--- src/fields/richText/populate.ts
+++ src/fields/richText/populate.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
-import { Collection } from '../../collections/config/types';
-import { RichTextField, Field } from '../config/types';
-import { PayloadRequest } from '../../express/types';
+import {Collection} from '../../collections/config/types';
+import {Field} from '../config/types';
+import {PayloadRequest} from '../../express/types';
+import {deepPick} from '../deepPick';
 
 type Arguments = {
   data: unknown
@@ -9,22 +10,23 @@ type Arguments = {
   key: string | number
   depth: number
   currentDepth?: number
-  field: RichTextField
+  field,
   req: PayloadRequest
   showHiddenFields: boolean
-}
+
+};
 
 export const populate = async ({
-  id,
-  collection,
-  data,
-  key,
-  overrideAccess,
-  depth,
-  currentDepth,
-  req,
-  showHiddenFields,
-}: Omit<Arguments, 'field'> & {
+                                 id,
+                                 collection,
+                                 data,
+                                 key,
+                                 overrideAccess,
+                                 depth,
+                                 currentDepth,
+                                 req,
+                                 showHiddenFields,
+                               }: Omit<Arguments, 'field'> & {
   id: string,
   field: Field
   collection: Collection
@@ -43,7 +45,21 @@ export const populate = async ({
   ]));
 
   if (doc) {
-    dataRef[key] = doc;
+    if (field.type === 'richText' && field.select && req.payloadAPI !== 'graphQL') {
+      const fieldsOrTrue = field.select({
+        data: doc,
+        collection: collection.config,
+        siblingData: dataRef,
+        req,
+      });
+      if (fieldsOrTrue !== true) {
+        dataRef[key] = deepPick(doc, ['id', ...fieldsOrTrue]);
+      } else {
+        dataRef[key] = doc;
+      }
+    } else {
+      dataRef[key] = doc;
+    }
   } else {
     dataRef[key] = null;
   }
